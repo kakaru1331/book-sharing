c19514524a2f39235faef806a780539bb0747a21
kakaru <kakaru1331@gmail.com>
2020-12-26 04:16:03 +0900

    Replace sqlite with object

--- pages/api/books.js
+++ pages/api/books.js
@@ -1,16 +1,5 @@
-import sqlite3 from 'sqlite3';
-import { open } from 'sqlite';
-import path from 'path'
-import getConfig from 'next/config'
-const { publicRuntimeConfig } = getConfig()
+import books from '../../data/books'
 
-export default async function getBooks(req, res) {
-  const db = await open({
-    filename: path.join(publicRuntimeConfig.PROJECT_ROOT, '/db/book-sharing.db'),
-    driver: sqlite3.Database
-  })
-
-  const books = await db.all(`select * from book`)
-
+export default function getBooks(req, res) {
   res.json(books)
 }

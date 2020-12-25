6aad2887e88beed9a3f2fc4806020fa164eb0656
kakaru <kakaru1331@gmail.com>
2020-12-26 04:14:59 +0900

    Add data of books

---
+++ data/books.js
@@ -0,0 +1,171 @@
+const books = [
+  {
+    bookID: 1,
+    title: '알고리즘 도감',
+    authors: '이시다 모리테루, 미야자키 쇼이치',
+    publishers: '제이펍',
+    imagePath: '/images/the-drawing-book-of-algorithm.jpg'
+  },
+  {
+    bookID: 2,
+    title: 'JavaScript Patterns',
+    authors: '스토얀 스테파노프',
+    publishers: '인사이트',
+    imagePath: '/images/javascript-patterns.jpg'
+  },
+  {
+    bookID: 3,
+    title: '포토샵 CC',
+    authors: '유윤자',
+    publishers: '아티오',
+    imagePath: '/images/photoshop-cc.jpg'
+  },
+  {
+    bookID: 4,
+    title: '초보자를 위한 안드로이드 앱 프로그래밍 입문',
+    authors: '유헌창, 정광식',
+    publishers: '배움터',
+    imagePath: '/images/android-getting-started.jpg'
+  },
+  {
+    bookID: 5,
+    title: 'Git 교과서',
+    authors: '이호진',
+    publishers: '길벗',
+    imagePath: '/images/git-textbook.jpg'
+  },  
+  {
+    bookID: 6,
+    title: 'Head First Design Patterns',
+    authors: '에릭 프리드먼 외',
+    publishers: '한빛미디어',
+    imagePath: '/images/head-first-design-patterns.jpg'
+  },
+  {
+    bookID: 7,
+    title: '자바스크립트는 왜 그모양일까?',
+    authors: '더글라스 크락포드',
+    publishers: '인사이트',
+    imagePath: '/images/how-javascript-works.jpg'
+  },
+  {
+    bookID: 8,
+    title: '테스트 주도 개발',
+    authors: '켄트 벡',
+    publishers: '인사이트',
+    imagePath: '/images/test-driven-development.jpg'
+  },
+  {
+    bookID: 9,
+    title: '인공지능',
+    authors: '이광형, 이병래',
+    publishers: 'KNOU PRESS',
+    imagePath: '/images/artificial-intelligence.jpg'
+  },
+  {
+    bookID: 10,
+    title: '클라우드 컴퓨팅',
+    authors: '정재화',
+    publishers: 'KNOU PRESS',
+    imagePath: '/images/cloud-computing.jpg'
+  },
+  {
+    bookID: 11,
+    title: '시뮬레이션',
+    authors: '김강현, 백두권',
+    publishers: 'KNOU PRESS',
+    imagePath: '/images/simulation.jpg'
+  },
+  {
+    bookID: 12,
+    title: 'UNIX 시스템',
+    authors: '김희천, 김진욱',
+    publishers: 'KNOU PRESS',
+    imagePath: '/images/unix-system.jpg'
+  },
+  {
+    bookID: 13,
+    title: '컴퓨터구조',
+    authors: '김형근, 손진곤',
+    publishers: 'KNOU PRESS',
+    imagePath: '/images/computer-architecture.jpg'
+  },{
+    bookID: 14,
+    title: '컴파일러 구성',
+    authors: '김강현, 박두순',
+    publishers: 'KNOU PRESS',
+    imagePath: '/images/compiler-configuration.jpg'
+  },
+  {
+    bookID: 15,
+    title: '알고리즘',
+    authors: '이관용, 김진욱',
+    publishers: 'KNOU PRESS',
+    imagePath: '/images/algorithm.jpg'
+  },
+  {
+    bookID: 16,
+    title: 'C++ 프로그래밍',
+    authors: '전중남, 이병래',
+    publishers: 'KNOU PRESS',
+    imagePath: '/images/c-plus-plus-programming.jpg'
+  },
+  {
+    bookID: 17,
+    title: '선형대수',
+    authors: '손진곤, 강태원',
+    publishers: 'KNOU PRESS',
+    imagePath: '/images/linear-algebra.jpg'
+  },
+  {
+    bookID: 18,
+    title: '철학의 이해',
+    authors: '이정호, 유현상, 박은미, 한길석',
+    publishers: 'KNOU PRESS',
+    imagePath: '/images/understanding-of-philosophy.jpg'
+  },
+  {
+    bookID: 19,
+    title: '컴퓨터과학개론',
+    authors: '이관용, 정광식',
+    publishers: 'KNOU PRESS',
+    imagePath: '/images/introduction-to-computer-science.jpg'
+  },
+  {
+    bookID: 20,
+    title: '컴퓨터 보안',
+    authors: '김진욱, 류대현, 김희천',
+    publishers: 'KNOU PRESS',
+    imagePath: '/images/computer-security.jpg'
+  },
+  {
+    bookID: 21,
+    title: '소프트웨어 공학',
+    authors: '김희천',
+    publishers: 'KNOU PRESS',
+    imagePath: '/images/software-engineering.jpg'
+  },
+  {
+    bookID: 22,
+    title: '컴퓨터 그래픽스',
+    authors: '이병래, 강현철, 김용규',
+    publishers: 'KNOU PRESS',
+    imagePath: '/images/computer-graphics.jpg'
+  },
+  {
+    bookID: 23,
+    title: '세상 읽기와 논술',
+    authors: '박정하, 박은미, 현남숙, 이현재, 이정호',
+    publishers: 'KNOU PRESS',
+    imagePath: '/images/reading-world-and-discuss.jpg'
+  },
+  {
+    bookID: 24,
+    title: '수학의 이해',
+    authors: '김승욱, 김영록, 박성호',
+    publishers: 'KNOU PRESS',
+    imagePath: '/images/understanding-to-math.jpg'
+  },
+]
+
+export default books

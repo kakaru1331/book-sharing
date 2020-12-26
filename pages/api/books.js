14556059763d0ed7636f725df645ab9aec8f9418
kakaru <kakaru1331@gmail.com>
2020-12-26 13:37:26 +0900

    Refactor books API

--- pages/api/books.js
+++ pages/api/books.js
@@ -1,5 +1,18 @@
-import books from '../../data/books'
+import prisma from '../../lib/prisma'
 
-export default function getBooks(req, res) {
+export default async function handle(req, res) {
+  if (req.method === 'GET') {
+    handleGET(res)
+  } else {
+    throw new Error(
+      `The HTTP ${req.method} method is not supported at this route.`
+    )
+  }
+}
+
+// GET /api/books
+async function handleGET(res) {
+  const books = await prisma.book.findMany()
+  
   res.json(books)
 }

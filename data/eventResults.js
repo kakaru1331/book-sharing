d2f5c02952ed7f65cace4719117ec93f3469f472
kakaru <kakaru1331@gmail.com>
2020-12-27 23:36:03 +0900

    Add event result data

---
+++ data/eventResults.js
@@ -0,0 +1,124 @@
+const eventResults = [
+  {
+    eventResultID: 1,
+    eventID: 1,
+    applicationID: 194
+  },
+  {
+    eventResultID: 2,
+    eventID: 2,
+    applicationID: 131
+  },
+  {
+    eventResultID: 3,
+    eventID: 3,
+    applicationID: 92
+  },
+  {
+    eventResultID: 4,
+    eventID: 4,
+    applicationID: 124
+  },
+  {
+    eventResultID: 5,
+    eventID: 5,
+    applicationID: 132
+  },
+  {
+    eventResultID: 6,
+    eventID: 6,
+    applicationID: 114
+  },
+  {
+    eventResultID: 7,
+    eventID: 7,
+    applicationID: 96
+  },
+  {
+    eventResultID: 8,
+    eventID: 8,
+    applicationID: 157
+  },
+  {
+    eventResultID: 9,
+    eventID: 9,
+    applicationID: 137
+  },
+  {
+    eventResultID: 10,
+    eventID: 10,
+    applicationID: 145
+  },
+  {
+    eventResultID: 11,
+    eventID: 11,
+    applicationID: 197
+  },
+  {
+    eventResultID: 12,
+    eventID: 12,
+    applicationID: 97
+  },
+  {
+    eventResultID: 13,
+    eventID: 13,
+    applicationID: 127
+  },
+  {
+    eventResultID: 14,
+    eventID: 14,
+    applicationID: 76
+  },
+  {
+    eventResultID: 15,
+    eventID: 15,
+    applicationID: 107
+  },
+  {
+    eventResultID: 16,
+    eventID: 16,
+    applicationID: 14
+  },
+  {
+    eventResultID: 17,
+    eventID: 17,
+    applicationID: 190
+  },
+  {
+    eventResultID: 18,
+    eventID: 18,
+    applicationID: 125
+  },
+  {
+    eventResultID: 19,
+    eventID: 19,
+    applicationID: 8
+  },
+  {
+    eventResultID: 20,
+    eventID: 20,
+    applicationID: 75
+  },
+  {
+    eventResultID: 21,
+    eventID: 21,
+    applicationID: 18
+  },
+  {
+    eventResultID: 22,
+    eventID: 22,
+    applicationID: 51
+  },
+  {
+    eventResultID: 23,
+    eventID: 23,
+    applicationID: 149
+  },
+  {
+    eventResultID: 24,
+    eventID: 24,
+    applicationID: 46
+  },
+]
+
+export default eventResults
